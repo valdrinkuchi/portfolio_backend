90ecf108115b4c39580334c83e611d8d296f5e59
Valdrin Kuchi <valdrinkuchi@gmail.com>
2021-11-02 22:31:29 +0100

    Add logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  # config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
@@ -70,7 +70,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  Rails.logger.level = 0
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
