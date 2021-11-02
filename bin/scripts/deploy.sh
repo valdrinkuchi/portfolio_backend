4bee689eaa8f3e52a8c5728d514db36d71dd83d7
Valdrin Kuchi <valdrinkuchi@gmail.com>
2021-11-02 20:08:13 +0100

    Fix credentials

--- bin/scripts/deploy.sh
+++ bin/scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
-docker-compose stop && docker-compose rm -f
-echo $MASTER_KEY > master.key
+docker-compose stop && docker rmi -f $(docker images -a -q)
+echo $MASTER_KEY > master.key,
+echo $CREDENTIALS > credentials.yml.enc
 docker-compose up -d
